d4218ede32d605da74554d90c12f1163c8b87bcf
SurabhiKeesara <surabhi.keesara@gmail>
2024-02-26 15:56:39 -0500

    poll timer exercise

--- eventloop/poll_timer.js
+++ eventloop/poll_timer.js
@@ -1,16 +1,18 @@
 const fs = require('fs');
 
-function someAsyncOperation(callback) {
+function someAsyncOperation(callback) { // takes ina  call back
+  // this is added to async event loop
   fs.readFile('./data/test.txt', callback);
 }
 
 const timeoutScheduled = Date.now();
 
+// Put on event loop, run after main thread is doe
 setTimeout(() => {
   const delay = Date.now() - timeoutScheduled;
 
   console.log(`${delay}ms have passed since I was scheduled`);
-}, 100);
+}, 2);
 
 // do someAsyncOperation which takes 95 ms to complete
 someAsyncOperation(() => {
